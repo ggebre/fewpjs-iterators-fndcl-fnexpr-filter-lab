3b1db35c3ac1feec86fe053f4453089d08fd8767
Getu Gebre <getu.gebre.gg@gmail.com>
2021-01-17 14:51:53 -0600

    done

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() == name.toLowerCase())
+}
+
+let fuzzyMatch = (drivers, word) => {
+    return drivers.filter(driver => driver.startsWith(word))
+} 
+
+function matchName(drivers, name){
+    return drivers.filter(driver => driver.name == name)
+}
